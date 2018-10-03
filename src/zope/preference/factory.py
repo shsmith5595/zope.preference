8a457bacc1b765f6ba6176a82a64896121cef2ea
Sheldon Smith <sheldon.smith@nextthought.com>
2018-10-03 09:52:15 -0500

    Move annotation logic to a factory

---
+++ src/zope/preference/factory.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import zope
+
+from zope.annotation import IAnnotations
+
+from zope.preference.interfaces import IPreferenceAnnotationFactory
+
+from zope.security.management import getInteraction
+
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+
+USER_PREFERENCE_ANNOTATION_KEY = 'zope.app.user.UserPreferences'
+
+
+@zope.interface.implementer(IPreferenceAnnotationFactory)
+class UserPreferenceAnnotationFactory(object):
+
+    annotation_key = USER_PREFERENCE_ANNOTATION_KEY
+
+    @property
+    def annotations(self):
+        # TODO: what if we have multiple participations?
+        principal = getInteraction().participations[0].principal
+        ann = zope.component.getMultiAdapter((principal, self), IAnnotations)
+        return ann
